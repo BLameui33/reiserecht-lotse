877938b41da4a58354c8c4e70aacfa8470e22c01
BLameui33 <alexanderdemirel@gmail.com>
2026-05-07 14:41:03 +0200

    cc

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -2,13 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 const baseUrl = 'https://fix-my-trip.com';
-const directoryPath = path.join(__dirname, 'fertige_seiten', 'de');
+
+// Live-Ordner für DE
+const directoryPath = path.join(__dirname, 'docs', 'de');
 
 function getHtmlFiles(dir, fileList = []) {
   const files = fs.readdirSync(dir);
 
   files.forEach(file => {
     const filePath = path.join(dir, file);
+
     if (fs.statSync(filePath).isDirectory()) {
       getHtmlFiles(filePath, fileList);
     } else if (file.endsWith('.html')) {
@@ -22,12 +25,14 @@ function getHtmlFiles(dir, fileList = []) {
 const files = getHtmlFiles(directoryPath);
 
 const urls = files.map(file => {
-  // relativer Pfad ab "fertige Seiten"
+  // relativer Pfad ab docs
   const relativePath = path
-    .relative(path.join(__dirname, 'fertige Seiten'), file)
+    .relative(path.join(__dirname, 'docs'), file)
     .replace(/\\/g, '/');
 
-  return `<url><loc>${baseUrl}/${relativePath}</loc></url>`;
+  return `  <url>
+    <loc>${baseUrl}/${relativePath}</loc>
+  </url>`;
 });
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
@@ -35,6 +40,7 @@ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 ${urls.join('\n')}
 </urlset>`;
 
-fs.writeFileSync('sitemap.xml', sitemap);
+// Sitemap direkt in docs speichern
+fs.writeFileSync(path.join(__dirname, 'docs', 'sitemap.xml'), sitemap);
 
-console.log('Sitemap nur für /fertige Seiten/de erstellt!');
+console.log('Sitemap für docs/de erstellt!');
